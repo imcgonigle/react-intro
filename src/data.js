2aed2ab39b80d0fabf2c009ea1ebb3135ec7d5b1
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2016-11-11 11:36:21 -0700

    ✨ Added todolist and name

---
+++ src/data.js
@@ -0,0 +1,16 @@
+const todos = [
+	{
+		text: "Learn React",
+		id: 1
+	},
+	{
+		text: "Ride a pony",
+		id: 2
+	}
+]
+
+const name = "Danny"
+
+module.exports = {
+	todos, name
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,9 @@
-console.log("hello")
+const ReactDOM = require("react-dom")
+const React = require("react")
+const App = require("./App.jsx")
+const data = require("./data.js")
+
+ReactDOM.render(
+  <App data={data} />,
+  document.getElementById('app')
+);
